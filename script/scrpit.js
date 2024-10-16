4fd0ee00660406262ec8e2dd103abd718f6bf5fd
jatoi <you@xwavenoormustafajatoi.com>
2024-10-16 15:02:48 +0500

    capstone project

---
+++ script/scrpit.js
@@ -0,0 +1,17 @@
+
+document.querySelector(".signInForm").addEventListener("submit",function (Event) {
+  Event.preventDefault();
+
+  let userEmail = document.getElementById("userEmail").value;
+  let userPassword = document.getElementById("userPassword").value;
+
+  console.log(userEmail, userPassword);
+  
+  if (userEmail && userPassword) {
+    alert("Register Successfully")
+  }else{
+    alert("Error")
+  }
+
+  window.location.href="./index.html";
+} );
